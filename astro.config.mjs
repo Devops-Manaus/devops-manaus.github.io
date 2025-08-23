90648dfb7d17ad1ead3cab807e29befe0c2db510
Samuel Viana da Silva <samuelviana2626@gmail.com>
2025-08-23 10:53:40 -0400

    updating path of astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import react from '@astrojs/react'
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://devopsmanaus.github.io/',
+    site: 'https://devops-manaus.github.io/',
     base: '/',
     output: 'static',
     integrations: [react()],
